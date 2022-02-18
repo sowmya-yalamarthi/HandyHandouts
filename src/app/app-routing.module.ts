01038bf4c867caea751fadb58b4f5f0a3f95579d
sowmya-yalamarthi <s542049@nwmissouri.edu>
2022-02-17 19:16:03 -0600

    added background image

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './components/content/content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ContentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+  
 })
 export class AppRoutingModule { }
